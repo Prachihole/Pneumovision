35cda9cb13bf44b1f6e7923c0293e51fe617d7e3
Prachihole <prachihole2006@gmail.com>
2026-07-19 12:05:44 +0530

    Configure app for Render deployment

--- app.py
+++ app.py
@@ -224,4 +224,4 @@ with gr.Blocks(css=css, title="PneumoVision Clinical") as demo:
     </div>
     """)
 
-demo.launch()
+demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
